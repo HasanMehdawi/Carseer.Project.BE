9441ce0a990ffba7d8e8fa3e779f27fb2b7ee951
Hasan Mihdawy <Hasan.Mihdawy@leading-point.com>
2025-01-24 22:34:41 +0300

    Add MakeResponse

--- Carseer.Project.BE.App/Carseer.Project.BE.Domain/Carseer.Project.BE.Domain/Class1.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace Carseer.Project.BE.Domain
-{
-    public class Class1
-    {
-
-    }
-}
---
+++ Carseer.Project.BE.App/Carseer.Project.BE.Domain/Carseer.Project.BE.Domain/V1/Responses/MakeResponse.cs
@@ -0,0 +1,15 @@
+﻿namespace Carseer.Project.BE.Domain.V1.Responses
+{
+    public class Make
+    {
+        public int Make_ID { get; set; }
+        public string? Make_Name { get; set; }
+    }
+
+    public class MakeResponse
+    {
+        public int Count { get; set; }
+        public string? Message { get; set; }
+        public List<Make>? Results { get; set; }
+    }
+}
